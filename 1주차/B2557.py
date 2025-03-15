ee6ddbb1395ac82ed9e83e928e4631bf23acf79f
Hyuk <dh95229505@gmail.com>
2025-03-15 18:22:21 +0900

    baekjoon no2557

--- 1주차/B2557.py
+++ 1주차/B2557.py
@@ -1,5 +1,19 @@
+'''
 import sys
+import time
 
-sys.stdout.write("Hello World!")
+s = time.perf_counter()
+sys.stdout.write("Hello World!\n")
+e = time.perf_counter()
+execution_time = e - s
+print(f"1:{execution_time}")
 
-print()
+s = time.perf_counter()
+print("Hello World!")
+e = time.perf_counter()
+execution_time = e - s
+print(f"2:{execution_time}")
+'''
+
+import sys
+sys.stdout.write("Hello World!\n")
